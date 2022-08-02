51dab206610ff708d470342e49c0ea31e2063274
Renan Cunha <renancdev@gmail.com>
2022-08-02 19:47:56 -0300

    Primeiro commit organo

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+export const Banner =  () => {
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="Banner principal"/>
+        </header>
+    )
+}
+
+export default Banner
